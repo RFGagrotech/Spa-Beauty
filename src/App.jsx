40f9366e5d5409b0c456a897c20f7e0b18ee5910
Ruben F.G <rfgagrotech@gmail.com>
2026-06-15 20:42:40 +0100

    Estructura html

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header.jsx";
+import Hero from "./components/Hero.jsx";
+import Testimonials from "./components/Testimonials";
+
+
+function App() {
+    return(
+        <>
+            <Header />
+            <Hero />
+            <Testimonials />
+        </>
+    )
+}
+
+export default App;
