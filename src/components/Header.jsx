8773b7acbade29d2d9c82c50b6d29196f089276f
Ruben F.G <rfgagrotech@gmail.com>
2026-06-17 20:14:53 +0100

    Estilo del header y nav desplegable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import logo from "../img/GlowQueen.svg";
 import vector from "../img/Vector1.svg";
 import search from "../img/Search.svg";
 import shopping from "../img/ShoppingBag.svg";
 
 function Header() {
+    const [menuOpen, setMenuOpen] = useState(false)
+
     return(
     <div className="container">
     <header className="header">
@@ -12,28 +15,40 @@ function Header() {
         <figcaption className="sr-only">Logotipo GlowQueen</figcaption>
       </figure>
 
+        <button
+          className="header__toggle"
+          type="button"
+          onClick={() => setMenuOpen(!menuOpen)}
+          aria-label="Abrir menú"
+          aria-expanded={menuOpen}
+        >
+          ☰
+        </button>
+        
 {/* Nav central */}
-      <nav className="center" aria-label="Principal">
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Services</a></li>
-          <li><a href="#">Facilities</a></li>
-          <li><a href="#">More <img src={vector} alt="" aria-hidden="true"/></a></li>
-        </ul>
-      </nav>
+      <div div className={`header__menu ${menuOpen ? "header__menu--open" : ""}`}>
+        <nav className="center" aria-label="Principal">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Facilities</a></li>
+            <li><a href="#">More <img src={vector} alt="" aria-hidden="true"/></a></li>
+          </ul>
+        </nav>
 
 {/*Iconos + auth*/}
-      <nav className="aside" aria-label="Acciones">
-        <ul className="icons">
-          <li><a href="#"><img src={search} alt="Buscar"/></a></li>
-          <li><a href="#"><img src={shopping} alt="Carrito"/></a></li>
-        </ul>
-        <ul className="auth">
-          <li><a href="#">Sign in</a></li>
-          <li><a href="#">Sign up</a></li>
-        </ul>
-      </nav>
+        <nav className="aside" aria-label="Acciones">
+          <ul className="icons">
+            <li><a href="#"><img src={search} alt="Buscar"/></a></li>
+            <li><a href="#"><img src={shopping} alt="Carrito"/></a></li>
+          </ul>
+          <ul className="auth">
+            <li><a href="#">Sign in</a></li>
+            <li><a href="#">Sign up</a></li>
+          </ul>
+        </nav>
+      </div>
     </header>
     </div>
     );
