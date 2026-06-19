7359b3a116e3a6e859d85bac9ae707e683c7ce29
Ruben F.G <rfgagrotech@gmail.com>
2026-06-20 00:16:46 +0100

    Correciones hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,35 +4,44 @@ import woman1 from "../img/woman2.png";
 import vector from "../img/Vector.svg";
 
 function Hero() {
-    return(
+  return (
     <section className="hero">
       <div className="hero-inner">
         <div className="hero-text">
           <h1>Bring back the beauty and glow of your skin</h1>
-          <p>We provide you various treatments from head to toe using the best product, advanced technology, 
-            and affordable price</p>
-          <div className="hero-actions">
-          <button type="button">Book now</button>
-          <button type="button">
-            <span className="circle"><img src={play} alt="" aria-hidden="true"/></span>
-            <span>Watch video</span>
-          </button>
-          </div>
-      </div>
 
-     {/*Bloque de imágenes del hero*/}
+          <p>
+            We provide you various treatments from head to toe using the best
+            product, advanced technology, and affordable price
+          </p>
+        </div>
+
         <div className="hero-media" aria-hidden="true">
           <div className="hero-panel"></div>
-          <img className="hero-woman" 
-          src={woman} 
-          srcSet={`${woman} 1x, ${woman1} 2x`} 
-          alt="Woman receiving beauty treatment"/>
-          <img className="hero-vector" src={vector} aria-hidden="true" />
+
+          <img
+            className="hero-woman"
+            src={woman}
+            srcSet={`${woman} 1x, ${woman1} 2x`}
+            alt="Woman receiving beauty treatment"
+          />
+
+          <img className="hero-vector" src={vector} alt="" aria-hidden="true" />
+        </div>
+
+        <div className="hero-actions">
+          <button type="button">Book now</button>
+
+          <button type="button">
+            <span className="circle">
+              <img src={play} alt="" aria-hidden="true" />
+            </span>
+            <span>Watch video</span>
+          </button>
         </div>
       </div>
     </section>
-
-    );
+  );
 }
 
 export default Hero;
